4ae479fc788775774b6a1dec85733b5fe42764d8
onn qazilbash <onn@ualberta.ca>
2020-06-29 16:28:31 -0600

    started some styling for nav bar

--- src/shared/Navigation/Navigation.js
+++ src/shared/Navigation/Navigation.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
+import './Navigation.css';
+import { Link } from 'react-router-dom';
+import Logo from '../images/stock.webp';
+
 function Navigation(){
     return (
-        <h1>This is the nav bar! Links go here</h1>
+        <div className="nav_bar">
+            
+            <img src={Logo} alt=""/>
+
+            <ul>
+                <li>
+                    <Link to='/home_page'>Home</Link>
+                </li>
+
+                <li>
+                    <Link to='/portfolio'>My Portfolios</Link>
+                </li>
+
+                <li>
+                    <Link to='/authenticate'>Login/Sign Up</Link>
+                </li>
+            </ul>
+        </div>
     );
 
 }
