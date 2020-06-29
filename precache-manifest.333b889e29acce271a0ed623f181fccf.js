289cb9bc2ba0f3607dbc006279877d2160d9badd
onn qazilbash <onn@ualberta.ca>
2020-06-29 15:09:58 -0600

    Updates

---
+++ precache-manifest.333b889e29acce271a0ed623f181fccf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d0827f0453b71f6fff041cdfa1cba5",
+    "url": "/paper_trader/index.html"
+  },
+  {
+    "revision": "d494ecd916a3f60c115d",
+    "url": "/paper_trader/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a019c3f2dd17066455d7",
+    "url": "/paper_trader/static/js/2.cf7608db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/paper_trader/static/js/2.cf7608db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d494ecd916a3f60c115d",
+    "url": "/paper_trader/static/js/main.10c4ebfb.chunk.js"
+  },
+  {
+    "revision": "7da67730ea4f9827091e",
+    "url": "/paper_trader/static/js/runtime-main.207e357a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/paper_trader/static/media/logo.5d5d9eef.svg"
+  }
+]);
