8d3aa6a9fce48ef3303ff3c84810dee5af5e43b9
onn qazilbash <onn@ualberta.ca>
2020-06-29 21:10:19 -0600

    fixed links and added some stylings

--- src/shared/Navigation/Navigation.js
+++ src/shared/Navigation/Navigation.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 import './Navigation.css';
-import { Link } from 'react-router-dom';
+import { Link, useHistory  } from 'react-router-dom';
 import Logo from '../images/stock.webp';
 
+
+
 function Navigation(){
+    let history = useHistory();
+    let redirect = () => {
+        history.push("/home_page");
+    }
     return (
         <div className="nav_bar">
             
-            <img src={Logo} alt=""/>
+            <img src={Logo} alt="" className = "logo" onClick={redirect}/>
             <nav>
                 <ul className = "nav__links">
                     <li>
