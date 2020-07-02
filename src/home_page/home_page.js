56dbee883b6c15c5f10c64d4cad67481311efc5c
onn qazilbash <onn@ualberta.ca>
2020-07-02 16:05:49 -0600

    added Stock graph to home_page only

--- src/home_page/home_page.js
+++ src/home_page/home_page.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import StockInfo from './StockInfo';
+
 import './home_page.css';
 
 function home_page(){
     return (
-        <h1>This is the home page!</h1>
+        <div className="home_page">
+            <h1>This is the home page!</h1>
+            <div className="stock_graph">
+                <StockInfo/>
+            </div>
+        </div>
     );
 
 
