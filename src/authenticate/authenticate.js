bab14eaf95c50fcdcd2a6bdae2c26041e6d3977b
onn qazilbash <onn@ualberta.ca>
2020-07-08 21:29:00 -0600

    added some inputs for auth page w/ styles

--- src/authenticate/authenticate.js
+++ src/authenticate/authenticate.js
@@ -2,12 +2,37 @@ import React from 'react';
 
 import './authenticate.css';
 
-function authenticate(){
-    return (
-        <h1>This is the authenticate page!</h1>
-    );
+export default class Authenticate extends React.Component{
+    constructor(props){
+        super(props);
+        // this.onChangeHandler = this.onChangeHandler.bind(this);
+        this.onSubmitHandler = this.onSubmitHandler.bind(this);
+        this.state = {email: "", password: ""};
+
+    }
+
+    onSubmitHandler(event){
+        event.preventDefault();
+        console.log(this.state);
+    }
+  
+
+
+    render(){
+        return (
+            <div className="auth__container">
+                <form onSubmit = {this.onSubmitHandler}>
+                    <h3>Enter your email</h3>
+                    <input type="text" onChange={(event)=> this.setState({email: event.target.value})}/>
+                    <h3>Enter your password</h3>
+                    <input type="text" onChange={(event)=> this.setState({password: event.target.value})}/>
+                </form>
+                <p className="signup__text">Don't have an account? Sign up here.</p>
+            </div>
+        );
+    }
 
 
 }
 
-export default authenticate;
+
