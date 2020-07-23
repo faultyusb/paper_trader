f579cc0af0ec81c4ee55b50262a22b8e22ccac6e
onn qazilbash <onn@ualberta.ca>
2020-07-22 21:28:33 -0600

    improvement: added error messages for invalid purchase/sell attempts

--- app_backend/routes/portfolio.js
+++ app_backend/routes/portfolio.js
@@ -5,15 +5,6 @@ const fetch = require('node-fetch');
 
 const Users = require('../models/Users');
 
-
-
-// router.get('/portfolios', ensureAuthenticated, (req, res) => {
-//     Users.findById(req.user._id)
-//         .then(user => {
-//             return res.json({ stocks: user.stocks });
-//     });
-// });
-
 router.put('/stocktrans', (req, res) => {
     if (!req.user){
         return res.json({ errorMessage: "You must be signed in for this feature." });
@@ -28,28 +19,51 @@ router.put('/stocktrans', (req, res) => {
 
     // if mode set to buy
     if (req.body.buy){
-        const stock = {
-            symbol: req.body.symbol,
-            price: req.body.price,
-            shares: req.body.shares,
-            volume: req.body.volume,
-            asset_value: req.body.price
-        };
-        console.log("Buying stocks!")
-        Users.findOneAndUpdate({ _id: req.user._id }, { $push: {stocks: stock} }, {useFindAndModify: false}, (err, result) => {
-            if (err){
-                console.log("Error in buying stocks.");
-            }
-        });
+
+        Users.findById(req.user._id)
+            .then(user => {
+                if (user.stocks.length === 5){
+                    return res.json({ errorMessage: "You cannot purchase more than 5 stocks." });
+                }
+
+                const stock = {
+                    symbol: req.body.symbol,
+                    price: req.body.price,
+                    shares: req.body.shares,
+                    volume: req.body.volume,
+                    asset_value: req.body.price
+                };
+                console.log("Buying stocks!")
+                Users.findOneAndUpdate({ _id: req.user._id }, { $push: {stocks: stock} }, {useFindAndModify: false}, (err, result) => {
+                    if (err){
+                        console.log("Error in buying stocks.");
+                    }
+                });
+
+            });
     }
 
     // if mode set to sell
     else{
-        console.log("Selling stock!");
-        Users.findOneAndUpdate({_id: req.user._id, "stocks.symbol": req.body.symbol}, {$set: {"stocks.$.shares": req.body.shares}}, {useFindAndModify: false}, (err, res)=>{
-            if (err){
-                console.log("Something wrong with selling stocks.");
+
+        Users.findOne({ _id: req.user._id})
+        .then(user => {
+            const total_stocks = user.stocks.filter(obj => obj.symbol===req.body.symbol);
+            let total_shares = 0;
+            total_stocks.forEach(stock => {
+                total_shares += stock.shares
+            });
+
+            if (req.body.shares > total_shares){
+                return res.json({ errorMessage: "Cannot sell more shares than you own." });
             }
+            
+            console.log("Selling stock!");
+            Users.findOneAndUpdate({_id: req.user._id, "stocks.symbol": req.body.symbol}, {$set: {"stocks.$.shares": req.body.shares}}, {useFindAndModify: false}, (err, res)=>{
+                if (err){
+                    console.log("Something wrong with selling stocks.");
+                }
+        });
         });
     }
 
