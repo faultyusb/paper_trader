2c3e5b325f87bcb5e250f36ecf5a79dbc6a192b6
onn qazilbash <onn@ualberta.ca>
2020-07-22 22:04:12 -0600

    refactor: removed old comments

--- app_backend/routes/users.js
+++ app_backend/routes/users.js
@@ -54,7 +54,7 @@ router.post('/SignUp', (req, res) => {
                         });
                     })
                 }
-
+               
             })
     }
 
@@ -74,7 +74,6 @@ router.post('/SignIn', function(req, res, next) {
       if (!user) { return res.json({ errorMessage: "Wrong username and/or password." }); }
       req.logIn(user, function(err) {
         if (err) { return next(err); }        
-        res.json({ message: "111 everything works" });
       });
     })(req, res, next);
   });
@@ -102,56 +101,7 @@ router.get('/isLoggedIn', (req, res) => {
 })
 
 
-// router.get('/portfolios', ensureAuthenticated, (req, res) => {
-// })
 
 module.exports = router;
 
 
-// router.put('/stocktrans', (req, res) => {
-
-//     if (!req.user){
-//         return res.json({ errorMessage: "You must be signed in for this feature." });
-//     }
-
-//     if (!req.body.symbol || !req.body.price || !req.body.shares || !req.body.volume){
-//         return res.json({ errorMessage: "Invalid inputs: Empty fields" });
-//     }
-//     if(req.body.shares < 1){
-//         return res.json({ errorMessage: "You must transact with at least 1 share." });
-//     }
-
-//     // mode set to buy
-//     console.log("trans start");
-//     if (req.body.buy){
-//         const stock = {
-//             symbol: req.body.symbol,
-//             price: req.body.price,
-//             shares: req.body.shares,
-//             volume: req.body.volume
-//         };
-//         console.log("Buying stocks!")
-//         Users.findOneAndUpdate({ _id: req.user._id }, { $push: {stocks: stock} }, {useFindAndModify: false}, (err, result) => {
-//             if (err){
-//                 console.log("something went wrong");
-//             }
-//         });
-//         res.json({ message: "hopefully the db is updated" });
-//     }
-
-//     // mode set to sell stocks
-//     else{
-//         console.log("Selling stock!");
-//         Users.findOneAndUpdate({_id: req.user._id, "stocks.symbol": req.body.symbol}, {$set: {"stocks.$.shares": req.body.shares}}, {useFindAndModify: false}, (err, res)=>{
-//             if (err){
-//                 console.log("yeet something wrong");
-//             }
-//         });
-//         res.json("hope this works");
-//     }
-//     console.log("trans end");
-
-
-
-
-// });
