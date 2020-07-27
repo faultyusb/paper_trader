0e975f055d4d4906dc436b3277ede6ade4724352
onn qazilbash <onn@ualberta.ca>
2020-07-26 21:25:21 -0600

    improvement: refactor

--- src/authenticate/signup.js
+++ src/authenticate/signup.js
@@ -92,8 +92,20 @@ class SignUp extends React.Component {
                 {this.state.error.errorStatus ? <Alert variant="warning"> {this.state.error.errorMessage} </Alert> : null}
                 {this.state.success.successStatus ? <Alert variant="primary"> {this.state.success.successMessage} </Alert> : null}
                 <div className="sign__up">
-                    <h2>Sign up! It's easy and free.</h2>
+                    <h2>Sign up!</h2>
                     <Form onSubmit = {this.onSubmitHandler}>
+                        <Form.Group as={Col} controlId="formGridAddress1">
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control placeholder="Kumail" onChange={(event)=> this.setState({first_name: event.target.value})}/>
+                        </Form.Group>
+
+                        <Form.Group as={Col}controlId="formGridAddress2">
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control placeholder="Qazilbash" onChange={(event)=> this.setState({last_name: event.target.value})}/>
+                        </Form.Group>
+
+
+
                         <Form.Group as={Col} controlId="formGridEmail">
                             <Form.Label>Email</Form.Label>
                             <Form.Control type="email" placeholder="Enter email" onChange={(event)=> this.setState({email: event.target.value})}/>
@@ -108,18 +120,7 @@ class SignUp extends React.Component {
                             <Form.Label>Please Re-enter your password</Form.Label>
                             <Form.Control type="password" placeholder="Password" onChange={(event)=> this.setState({password2: event.target.value})}/>
                         </Form.Group>
-
                         
-                        <Form.Group as={Col} controlId="formGridAddress1">
-                            <Form.Label>First Name</Form.Label>
-                            <Form.Control placeholder="John" onChange={(event)=> this.setState({first_name: event.target.value})}/>
-                        </Form.Group>
-
-                        <Form.Group as={Col}controlId="formGridAddress2">
-                            <Form.Label>Last Name</Form.Label>
-                            <Form.Control placeholder="Appleseed" onChange={(event)=> this.setState({last_name: event.target.value})}/>
-                        </Form.Group>
-
                         <Col xs="auto">
                             <Button variant="primary" type="submit">
                                 Submit
