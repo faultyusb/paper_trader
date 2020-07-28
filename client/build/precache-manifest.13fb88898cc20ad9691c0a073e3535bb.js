02cf73d5dc01c2de8e01429efc0678adf284bba4
onn qazilbash <onn@ualberta.ca>
2020-07-28 01:03:43 -0600

    deploy to heroku

---
+++ client/build/precache-manifest.13fb88898cc20ad9691c0a073e3535bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad5393c17a4c792f121748868831f81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a0eaed758c92c111acd",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e8e444be96a71743b8f2",
+    "url": "/static/css/main.e5dd190c.chunk.css"
+  },
+  {
+    "revision": "6a0eaed758c92c111acd",
+    "url": "/static/js/2.62fed337.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.62fed337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e444be96a71743b8f2",
+    "url": "/static/js/main.6a643fdc.chunk.js"
+  },
+  {
+    "revision": "1ca578e9c41e0c81ee2c",
+    "url": "/static/js/runtime-main.c6e50904.js"
+  },
+  {
+    "revision": "d21bdf173ecf8d898c7b85efe6d52535",
+    "url": "/static/media/stock_logo.d21bdf17.jpg"
+  }
+]);
