d5e875cd524f8093bea9ac7f9481bdd4062a79d0
onn qazilbash <onn@ualberta.ca>
2020-07-28 21:20:10 -0600

    ready for dep

---
+++ client/build/precache-manifest.a3e0ef7264b7bb3c619c3c7f8990b563.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b5704f0baed20716fd7c68b5af6831",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a0eaed758c92c111acd",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "b84b8ae0eedddf75d9e6",
+    "url": "/static/css/main.e5dd190c.chunk.css"
+  },
+  {
+    "revision": "6a0eaed758c92c111acd",
+    "url": "/static/js/2.62fed337.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.62fed337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b84b8ae0eedddf75d9e6",
+    "url": "/static/js/main.d7c06247.chunk.js"
+  },
+  {
+    "revision": "1ca578e9c41e0c81ee2c",
+    "url": "/static/js/runtime-main.c6e50904.js"
+  },
+  {
+    "revision": "d21bdf173ecf8d898c7b85efe6d52535",
+    "url": "/static/media/stock_logo.d21bdf17.jpg"
+  }
+]);
